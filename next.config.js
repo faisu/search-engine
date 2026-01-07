67e044262ed563e6ead0ff8088593a658b395417
DonChandu98 <ansarimunib1234@gmail.com>
2026-01-07 16:15:21 +0530

    Fix webpack config to handle @napi-rs/canvas native modules

--- next.config.js
+++ next.config.js
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer, webpack }) => {
+    if (isServer) {
+      // Ignore .node files completely - they're native bindings
+      config.plugins.push(
+        new webpack.IgnorePlugin({
+          resourceRegExp: /\.node$/,
+        })
+      );
+      
+      // Make @napi-rs/canvas external so it's not bundled
+      config.externals = config.externals || [];
+      config.externals.push('@napi-rs/canvas');
+    }
+    
+    return config;
+  },
+}
 
 module.exports = nextConfig
 
